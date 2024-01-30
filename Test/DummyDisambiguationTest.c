2a176b0101bd247a0a5f64c1d060fe5824489294
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2024-01-30 19:47:44 +0300

    Modified test code.

--- Test/DummyDisambiguationTest.c
+++ Test/DummyDisambiguationTest.c
@@ -7,12 +7,14 @@
 #include <DisambiguationCorpus.h>
 #include <DisambiguatedWord.h>
 #include <Memory/Memory.h>
+#include <string.h>
 #include "../src/DummyDisambiguation.h"
 
 int main(){
     Fsm_morphological_analyzer_ptr fsm = create_fsm_morphological_analyzer3();
     Corpus_ptr corpus = create_disambiguation_corpus("penntreebank.txt");
-    for (int i = 0; i < corpus->sentences->size; i++){
+    int correct_parse = 0, total_parse = 0;
+    for (int i = 0; i < 10; i++){
         Sentence_ptr sentence = array_list_get(corpus->sentences, i);
         Sentence_ptr new_sentence = create_sentence();
         for (int j = 0; j < sentence->words->size; j++){
@@ -25,6 +27,14 @@ int main(){
         for (int j = 0; j < sentence->words->size; j++){
             Disambiguated_word_ptr word1 = array_list_get(sentence->words, j);
             Fsm_parse_ptr fsm_parse = array_list_get(correct_parses, j);
+            char* list1 = transition_list(fsm_parse);
+            char* list2 = morphological_parse_to_string(word1->parse);
+            if (strcmp(list1, list2) == 0){
+                correct_parse++;
+            }
+            total_parse++;
+            free_(list1);
+            free_(list2);
             free_fsm_parse_list(fsm_parses[j]);
         }
         free_(fsm_parses);
